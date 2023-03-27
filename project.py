bab72b611b3a9df22c9f51b0eed16738c5fb09a3
Esther1605 <125884386+Esther1605@users.noreply.github.com>
2023-03-27 10:50:14 +0100

    Add files via upload

---
+++ project.py
@@ -0,0 +1,12 @@
+
+#     Generate  complete_url
+def generate_url(city):
+
+    base_id = "http://api.openweathermap.org/data/2.5/weather?"
+    api_key = "a1d832c101f04481e98ece5a0a6cb290"
+
+    complete_url = base_id + "appid="+api_key+"&q=" + city
+    return (complete_url)
+
+
+print(generate_url("Lagos"))
