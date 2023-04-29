788830882279762d50de027ebf362043ea73e8bf
Femprogs <98106415+Femprogs@users.noreply.github.com>
2023-04-29 17:08:42 +0100

    added the weather_utility code

--- weather.py
+++ weather.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import requests
 from project import generate_url
+from weather_utility import get_country_name, get_temperature
 
-# import json
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
+# @app.route('/<city>/<country>/<temperature>', methods =['GET', 'POST'])
 def index():
     return render_template('index.html')
 
@@ -16,25 +18,14 @@ def index():
 def weather():
     if request.method == 'POST':
         city = request.form['city']
-        print(city)
+        if not city:
+            return jsonify({'error': 'Please provide a city name.'}), 400
         url = generate_url(city)
-        print(url)
         response = requests.get(url)
         data = response.json()
-        print(data)
-    return redirect(url_for('index'))
-    # city = request.args.get('city')
-    # if not city:
-    # return jsonify({'error': 'Please provide a city name.'}), 400
-
-
-# url = 'complete_url'
-
-# response = requests.get(url)
-# data = response.json()
-
-# if data['cod'] == '404':
-    # return jsonify({'error': 'City not found.'}), 404
+        country = get_country_name(data)
+        temperature = get_temperature(data)
+        return render_template('index.html', city=city, country=country, temperature=temperature)
 
 
 if __name__ == '__main__':
